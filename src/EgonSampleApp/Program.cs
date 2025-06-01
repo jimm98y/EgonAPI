3ebca78376cc9dfd49a7fa1d52830e7d4f6e0be8
Lukas Volf <jimm98y@hotmail.com>
2025-06-01 23:40:56 +0200

    F Added sample app

---
+++ src/EgonSampleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using EgonAPI;
+using System;
+
+var device = await EgonClient.DiscoverAsync("192.168.1.255");
+if (device != null)
+    Console.WriteLine($"Device found");
+else
+    Console.WriteLine($"No device found");
